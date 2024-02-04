3c82697777bf7d129a7ae890d3c210ab7f4d1fa1
noface <noface@outlook.com>
2024-02-04 18:59:05 +0100

    testing

--- .github/workflows/binary/entrypoint.sh
+++ .github/workflows/binary/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /root/.config/guix/current/bin/guix-daemon --build-users-group=guixbuild &
-evn FORCE_DIRTY_WORKTREE=true ./contrib/guix/guix-build
+git config --global --add safe.directory ${GITHUB_WORKSPACE}
+env FORCE_DIRTY_WORKTREE=true ./contrib/guix/guix-build
 source "contrib/guix/libexec/prelude.bash"
 echo "outdir=${OUTDIR_BASE:-${VERSION_BASE}/output}/${INPUT_HOST}" >> $GITHUB_OUTPUT
